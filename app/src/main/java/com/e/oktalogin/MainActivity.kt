805b519d7f45afb75d02d6590392632e5fb266ab
Fer Fuentes <lzfer1@me.com>
2021-08-23 12:01:35 -0700

    Added navigation component

---
+++ app/src/main/java/com/e/oktalogin/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.e.oktalogin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.e.oktalogin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigationComponent()
+    }
+
+    private fun setupNavigationComponent() {
+        navController = (
+                supportFragmentManager.findFragmentById(binding.navHostContainer.id) as NavHostFragment
+                ).navController
+    }
+}
